f3246048e4ae4d52f3992e0cfe85b29920af2415
Caleb Carlson <ccarlson355@gmail.com>
2023-02-11 21:40:16 -0700

    looping trimpot measurements

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 
 import os
 import time
+import sys
+import signal
 import busio
 import digitalio
 import board
@@ -9,7 +11,16 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 
+def signal_handler(sig, frame):
+    print("\nExiting\n")
+    sys.exit(0)
+
+
 def main():
+
+    # Create SIGINT handler for graceful shutdowns
+    signal.signal(signal.SIGINT, signal_handler)
+
     # create the spi bus
     spi_bus = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -22,13 +33,17 @@ def main():
     # create an analog input channel on pin 0
     input_channel = AnalogIn(mcp, MCP.P0)
 
-    print('Raw ADC Value: ', input_channel.value)
-    print('ADC Voltage: ' + str(input_channel.voltage) + 'V')
+    print('Initial ADC Value: ', input_channel.value)
+    print('Initial ADC Voltage: ' + str(input_channel.voltage) + 'V')
 
-    last_read = 0       # this keeps track of the last potentiometer value
-    tolerance = 250     # to keep from being jittery we'll only change
-                        # volume when the pot has moved a significant amount
-                        # on a 16-bit ADC
+    last_read = -1
+    tolerance = 250
+
+    while True:
+        current_value = input_channel.value
+        if abs(current_value - last_read) > tolerance:
+            print(f"value={current_value},voltage={input_channel.voltage}")
+            last_read = current_value
 
 
 if __name__ == "__main__":
