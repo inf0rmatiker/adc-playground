9d86c9e8ec8fb3c1f64ab796b58053528f7dbda3
Caleb Carlson <ccarlson355@gmail.com>
2023-02-11 22:20:37 -0700

    Save buffers

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import signal
 import busio
 import digitalio
 import board
+import numpy as np
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
@@ -14,6 +15,22 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 def signal_handler(sig, frame):
     print("\nExiting\n")
     sys.exit(0)
+
+
+def capture_buffer(input_channel: AnalogIn, filename: str):
+
+    # capture 1000 samples
+    # samples = np.empty(1000, dtype=np.ushort)
+    # for i in range(1000):
+    #     samples[i] = np.ushort(input_channel.value)
+    
+    # np.savetxt(filename, samples, fmt="%d", delimiter=",")
+
+    samples=np.empty(1000, dtype=np.float_)
+    for i in range(1000):
+        samples[i] = np.float_(input_channel.voltage)
+
+    np.savetxt(filename, samples, fmt="%.5f", delimiter=",")
 
 
 def main():
@@ -36,14 +53,11 @@ def main():
     print('Initial ADC Value: ', input_channel.value)
     print('Initial ADC Voltage: ' + str(input_channel.voltage) + 'V')
 
-    last_read = -1
-    tolerance = 250
 
-    while True:
-        current_value = input_channel.value
-        if abs(current_value - last_read) > tolerance:
-            print(f"value={current_value},voltage={input_channel.voltage}")
-            last_read = current_value
+    for i in range(10):
+        capture_buffer(input_channel, f"buffer_{i}.csv")
+
+    print("Done")
 
 
 if __name__ == "__main__":
